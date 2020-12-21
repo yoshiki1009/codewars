7e6c87d6fc61fb7dc5396c05806605bdeec91092
Yoshiki Sakai <y.s.west1009@gmail.com>
2020-12-21 12:14:36 -0800

    add new katas

---
+++ 6_kyu/find_the_odd_int.js
@@ -0,0 +1,19 @@
+/* Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times. */
+
+function findOdd(A) {
+    //happy coding!
+    let count = 0;
+    let arr = A.sort((a, b) => a - b);
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[i] == arr[j]) {
+          count++;
+        }
+      }
+      if (count % 2 !== 0) {
+        return arr[i];
+      }
+    }
+  }
