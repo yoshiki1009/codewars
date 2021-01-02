6ac7c1f9df838793031c0ced43b91c1abcde0988
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-01-01 17:58:47 -0800

    add katas

---
+++ 7_kyu/isograms.js
@@ -0,0 +1,14 @@
+/* An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+isIsogram("Dermatoglyphics") == true
+isIsogram("aba") == false
+isIsogram("moOse") == false // -- ignore letter case */
+
+function isIsogram(str){
+    str = str.toLowerCase();
+    for(let i = 0; i < str.length; i++)
+      for(let j = i + 1; j < str.length; j++)
+        if(str[i] === str[j]){
+          return false;}
+          return true;
+  }
