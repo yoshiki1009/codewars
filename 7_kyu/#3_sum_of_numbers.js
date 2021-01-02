f99fbadf9c10b9ba0effbaa6ee4c530ad112a251
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-01-02 12:57:48 -0800

    add katas

---
+++ 7_kyu/#3_sum_of_numbers.js
@@ -0,0 +1,17 @@
+/* Given two integers a and b, which can be positive or negative, find the sum of all the integers between including them too and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples
+GetSum(1, 0) == 1   // 1 + 0 = 1
+GetSum(1, 2) == 3   // 1 + 2 = 3
+GetSum(0, 1) == 1   // 0 + 1 = 1
+GetSum(1, 1) == 1   // 1 Since both are same
+GetSum(-1, 0) == -1 // -1 + 0 = -1
+GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2 */
+
+const getSum = (a, b) => {
+    let min = Math.min(a, b),
+        max = Math.max(a, b);
+    return (max - min + 1) * (min + max) / 2;
+  }
