5c3d85b8741102537a176d4f298fed0ddb82a794
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-01-07 07:26:32 -0800

    add katas

---
+++ 8_kyu/convert_boolean_values_to_strings.js
@@ -0,0 +1,9 @@
+/* Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+FUNDAMENTALSBOOLEANSBEST PRACTICES */
+
+function boolToWord( bool ){
+    if(bool){
+      return "Yes"
+    } return 'No'
+  }
