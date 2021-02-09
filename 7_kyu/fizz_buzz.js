62b292a98dcf43583186acaf845a27fba9172a98
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-02-09 07:07:16 -0800

    add katas

---
+++ 7_kyu/fizz_buzz.js
@@ -0,0 +1,26 @@
+Return an array containing the numbers from 1 to N, where N is the parametered value. N will never be less than 1 (in C#, N might be less then 1).
+
+Replace certain values however if any of the following conditions are met:
+
+If the value is a multiple of 3: use the value "Fizz" instead
+If the value is a multiple of 5: use the value "Buzz" instead
+If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+Method calling example:
+
+fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+
+function fizzbuzz(n){
+    let arr=[]
+    for(let i=1; i<=n; i++){
+      if(i%3==0 && i%5!= 0){
+          arr.push('Fizz')
+      }else if(i%3!=0 && i%5== 0){
+          arr.push('Buzz')
+      }else if(i%3==0 && i%5== 0){
+        arr.push('FizzBuzz')
+      }else{
+        arr.push(i)
+      }
+    } return arr
+  }
