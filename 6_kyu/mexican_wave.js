476dfbf0c06ff363399fc42526d529d6af1705b2
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-05-13 09:30:30 -0700

    add katas

---
+++ 6_kyu/mexican_wave.js
@@ -0,0 +1,18 @@
+/* Rules
+ 1.  The input string will always be lower case but maybe empty.
+
+ 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+Example
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"
+ */
+function wave(str){
+    let newArr = [];
+    for (let i = 0; i < str.length; i++) {
+      let copy = str.split('');
+      if(copy[i] !== ' ') {
+      copy[i] = copy[i].toUpperCase()
+      newArr.push(copy.join(''))
+      }
+    }
+    return newArr
+  }
