62ca2f094fd318846758f93cecba5e56cb3c704a
Yoshiki Sakai <y.s.west1009@gmail.com>
2021-06-21 22:44:05 -0700

    add katas

--- 7_kyu/over_the_world.js
+++ 7_kyu/over_the_world.js
@@ -1,4 +1,4 @@
-/* Task
+/*
 You've just moved into a perfectly straight street with exactly n identical houses on either side of the road. Naturally, you would like to find out the house number of the people on the other side of the street. The street looks something like this:
 
 Street
